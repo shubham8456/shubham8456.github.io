8733a34388afc8895e5ed90532b7c0b181e24f64
Shubham Rawat <shubham84567@gmail.com>
2026-04-08 03:21:24 +0530

    pushed local export for github pages

---
+++ out/_next/static/e6oTzZFDQe5ZXVwZVkLH3/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/portfolio.github.io//_next/:path+",
+        "destination": "/portfolio.github.io/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
